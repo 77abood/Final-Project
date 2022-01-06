977322d2bd9de059a0ecee7dc15ac754fa026cc3
FadiBadarni <Fadybd1@gmail.com>
2022-01-06 23:20:50 +0500

    user registration, user sign in, user sign out, profile update

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from authentication import views as authentication_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='Web-Home'),
     path('about/', views.about, name='About-Page'),
     path('FAQ/', views.faq, name='FAQ-Page'),
     path('contact/', views.contact, name='Contact-Page'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
