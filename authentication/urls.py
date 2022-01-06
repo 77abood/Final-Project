97d27de080f56a126a9781e73f1bfe757d280817
77abood <abed.18.salem@gmail.com>
2022-01-06 23:24:37 +0500

    removed some points

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='About-Page'),
     path('FAQ/', views.faq, name='FAQ-Page'),
     path('contact/', views.contact, name='Contact-Page'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
 
